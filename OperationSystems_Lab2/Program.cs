cc5cbf7bb5c2cfe545ed2d3abdb0f04fede88858
Nazar Lavrentiuk <72525276+nazarlvr@users.noreply.github.com>
2022-06-24 16:50:52 +0300

    OperationSystems_Lab2

---
+++ OperationSystems_Lab2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Lab2OS
+{
+    class Program
+    {
+        public static bool ConnectToServer(Socket s, int x, char ch, int OurPort)
+        {
+            Console.WriteLine("Client Started");
+            s.Connect("127.0.0.1", OurPort);
+            Console.WriteLine("Client Connected to server " + ch);
+            byte[] bytes = Encoding.ASCII.GetBytes(x.ToString());
+            s.Send(bytes);
+            byte[] ans = new byte[256];
+            s.Receive(ans);
+            Console.WriteLine("Server " + ch + " returned:" + " " + BitConverter.ToBoolean(ans).ToString());
+            return BitConverter.ToBoolean(ans);
+        }
+        static Socket sockF = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static Socket sockG = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static int FPort = 1034;
+        static int GPort = 1035;
+        private static int X;
+        static void Main(string[] args)
+        {
+            bool fres, gres;
+            Console.WriteLine("Input x");
+            X = int.Parse(Console.ReadLine());
+            Task<bool> taskF = new Task<bool>(() => ConnectToServer(sockF, X, 'F', FPort));
+            Task<bool> taskG = new Task<bool>(() => ConnectToServer(sockG, X, 'G', GPort));
+            taskF.Start();
+            taskG.Start();
+            bool flag = true;
+            while (flag)
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine("Print 1 (Stop) or 2 (Continue) or 3 (don't ask again) or 4 (change x)");
+                int key = int.Parse(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        flag = false;
+                        break;
+                    case 2:
+                        if (taskF.IsCompleted && taskG.IsCompleted)
+                        {
+                            Console.WriteLine("Result of f||g using Sockets is " + (taskF.Result || taskG.Result));
+                            flag = false;
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Result of f||g using Sockets is " + (taskF.Result || taskG.Result));
+                        flag = false;
+                        break;
+                    case 4:
+                        Console.WriteLine("Input new value of x");
+                        X = int.Parse(Console.ReadLine());
+                        goto case 2;
+                }
+            }
+            Console.WriteLine("End of execution");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OperationSystems_Lab2/ServerF/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerF
+{
+    class Program
+    {
+        public static bool f(int x) {
+            bool res = x * (x - 20) > 0;
+            Thread.Sleep(12000);
+            if (x > 10 && x < 20) 
+            { 
+                Thread.Sleep(100000); 
+                return false; 
+            }
+            return res;
+        } 
+        static int OurPort = 1034;
+        static void Send(string message, Socket temp)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            temp.Send(data);
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Console.WriteLine("Server started");
+                listenSocket.Bind(new IPEndPoint(IPAddress.Any, OurPort));
+                listenSocket.Listen(1);
+                Socket temperary = listenSocket.Accept();
+                string s = "";
+                bool flag = true;
+                while (true)
+                {
+                    Console.WriteLine("New Connection");
+                    byte[] bytes = new byte[256];
+                    int len = temperary.Receive(bytes);
+                    s = Encoding.ASCII.GetString(bytes, 0, len);
+                    int x = Int32.Parse(s);
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + " " + s);
+                    bool answer = f(x);
+                    byte[] ans = new byte[256];
+                    ans = BitConverter.GetBytes(answer);
+                    temperary.Send(ans);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
